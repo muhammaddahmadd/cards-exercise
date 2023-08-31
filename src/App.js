ea46a7b4269590bca9f334476f650dc58d86317e
Muhammad <ranaahmad131@gmail.com>
2023-08-31 21:25:44 +0500

    Fun Exercise

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <FlashCards />
+    </div>
+  );
+}
+
+const questions = [
+  {
+    id: 3457,
+    question: "What language is React based on?",
+    answer: "JavaScript",
+  },
+  {
+    id: 7336,
+    question: "What are the building blocks of React apps?",
+    answer: "Components",
+  },
+  {
+    id: 8832,
+    question: "What's the name of the syntax we use to describe a UI in React?",
+    answer: "JSX",
+  },
+  {
+    id: 1297,
+    question: "How to pass data from parent to child components?",
+    answer: "Props",
+  },
+  {
+    id: 9103,
+    question: "How to give components memory?",
+    answer: "useState hook",
+  },
+  {
+    id: 2002,
+    question:
+      "What do we call an input element that is completely synchronised with state?",
+    answer: "Controlled element",
+  },
+];
+
+function FlashCards() {
+  const [selectedId, setSelectedId] = useState(7336);
+
+  // function handleClick(id){
+  //   setSelectedId(id !== selectedId ? id : null)
+  // }
+
+  return (
+    <div className="flashcards">
+      {questions.map((qs) => (
+        <div
+          className={qs.id === selectedId ? "selected" : ""}
+          key={qs.id}
+          onClick={() => setSelectedId(qs.id !== selectedId ? qs.id : null)}
         >
-          Learn React
-        </a>
-      </header>
+          <p>{qs.id === selectedId ? qs.answer : qs.question}</p>
+        </div>
+      ))}
     </div>
   );
 }
